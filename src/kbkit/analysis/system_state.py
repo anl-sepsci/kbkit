4abd1ab15d485b5e0be3794d570714d100fe37c2
aperoutka <allison.peroutka98@gmail.com>
2025-08-29 09:36:23 -0500

    added documentation

--- src/kbkit/analysis/system_state.py
+++ src/kbkit/analysis/system_state.py
@@ -5,6 +5,7 @@ from functools import cached_property
 
 import numpy as np
 from numpy.typing import NDArray
+from pint import UnitRegistry
 
 from kbkit.config.unit_registry import load_unit_registry
 from kbkit.schema.system_config import SystemConfig
@@ -23,6 +24,13 @@ class SystemState:
     config: SystemConfig
         System configuration for a set of systems.
 
+        
+    Attributes
+    ----------
+    ureg: UnitRegistry
+        Pint unit registry.
+    Q_: UnitRegistry.Quantity
+        Pint quantity object for unit conversions.
     """
 
     def __init__(self, config: SystemConfig) -> None:
@@ -35,7 +43,7 @@ class SystemState:
 
     @property
     def top_molecules(self) -> list[str]:
-        """list: Unique molecules in topology files."""
+        """list[str]: Unique molecules in topology files."""
         return self.config.molecules
 
     @property
@@ -45,7 +53,7 @@ class SystemState:
 
     @cached_property
     def salt_pairs(self) -> list[tuple[str, str]]:
-        """list: List of salt pairs as (cation, anion) tuples."""
+        """list[tuple[str, str]]: List of salt pairs as (cation, anion) tuples."""
         # get unique combination of anions/cations in configuration
         salt_pairs = [(cation, anion) for cation, anion in itertools.product(self.config.cations, self.config.anions)]
 
@@ -59,18 +67,18 @@ class SystemState:
 
     @cached_property
     def _nosalt_molecules(self) -> list[str]:
-        """list: Molecules not part of any salt pair."""
+        """list[str]: Molecules not part of any salt pair."""
         paired = {mol for pair in self.salt_pairs for mol in pair}
         return [mol for mol in self.top_molecules if mol not in paired]
 
     @cached_property
     def _salt_molecules(self) -> list[str]:
-        """list: Combined molecule names for each salt pair."""
+        """list[str]: Combined molecule names for each salt pair."""
         return [".".join(pair) for pair in self.salt_pairs]
 
     @cached_property
     def unique_molecules(self) -> list[str]:
-        """list: Molecules present after combining salt pairs as single entries."""
+        """list[str]: Molecules present after combining salt pairs as single entries."""
         return self._nosalt_molecules + self._salt_molecules
 
     def _get_mol_idx(self, mol: str, molecule_list: list[str]) -> int:
@@ -88,7 +96,7 @@ class SystemState:
 
     @property
     def n_comp(self) -> int:
-        """int: Number of unique components (molecules) in all systems."""
+        """int: Total number of unique components."""
         return len(self.unique_molecules)
 
     @cached_property
@@ -163,15 +171,48 @@ class SystemState:
         return self.molecule_counts / self.molecule_counts.sum(axis=1)[:, np.newaxis]
 
     def temperature(self, units: str = "K") -> NDArray[np.float64]:
-        """Temperature of each simulation."""
+        """Temperature of each simulation.
+        
+        Parameters
+        ----------
+        units: str
+            Temperature units (default: K)
+            
+        Returns
+        -------
+        np.ndarray
+            1D temperature array as a function of composition.
+        """
         return np.array([meta.props.get("temperature", units=units) for meta in self.config.registry])
 
     def volume(self, units: str = "nm^3") -> NDArray[np.float64]:
-        """Volume of each simulation."""
+        """Volume of each simulation.
+        
+        Parameters
+        ----------
+        units: str
+            Volume units (default: nm^3)
+            
+        Returns
+        -------
+        np.ndarray
+            1D volume array as a function of composition.
+        """
         return np.array([meta.props.get("volume", units=units) for meta in self.config.registry])
 
     def molar_volume(self, units: str = "nm^3 / molecule") -> NDArray[np.float64]:
-        """Molar volumes of pure components."""
+        """Molar volumes of pure components.
+        
+        Parameters
+        ----------
+        units: str
+            Molar volume units (default: nm^3/molecule)
+            
+        Returns
+        -------
+        np.ndarray
+            1D array for each unique molecule.
+        """
         vol_unit, N_unit = units.split("/")
         volumes = self.volume(vol_unit)
         # make dict in same order as top molecules
@@ -186,11 +227,33 @@ class SystemState:
         return np.fromiter(volumes_map.values(), dtype=np.float64)
 
     def enthalpy(self, units: str = "kJ/mol") -> NDArray[np.float64]:
-        """Enthalpy of each simulation."""
+        """Enthalpy of each simulation.
+        
+        Parameters
+        ----------
+        units: str
+            Enthalpy units (default: kJ/mol/K)
+            
+        Returns
+        -------
+        np.ndarray
+            1D array of system enthalpies as a function of composition.
+        """
         return np.array([meta.props.get("enthalpy", units=units) for meta in self.config.registry])
 
     def pure_enthalpy(self, units: str = "kJ/mol") -> NDArray[np.float64]:
-        """Pure component enthalpies."""
+        """Pure component enthalpies.
+        
+        Parameters
+        ----------
+        units: str
+            Enthalpy units (default: kJ/mol/K)
+            
+        Returns
+        -------
+        np.ndarray
+            1D array of enthalpies for pure components.
+        """
         enth: dict[str, float] = dict.fromkeys(self.pure_molecules, 0)
         for meta in self.config.registry:
             if meta.kind == "pure":
@@ -203,29 +266,109 @@ class SystemState:
         return np.fromiter(enth.values(), dtype=np.float64)
 
     def ideal_enthalpy(self, units: str = "kJ/mol") -> NDArray[np.float64]:
-        """Linear combination of simulation enthalpy."""
+        """Ideal enthalpy as a function of composition.
+        
+        Parameters
+        ----------
+        units: str
+            Enthalpy units (default: kJ/mol/K)
+            
+        Returns
+        -------
+        np.ndarray
+            1D array of ideal enthalpies as a function of composition.
+
+        .. math::
+            H^{id} = \sum_{i=1}^n x_i H_i
+        
+        where: 
+            - :math:`x_i` is mol fraction of molecule :math:`i`
+            - :math:`H_i` is the pure component simulation enthalpy of molecule :math:`i`
+        """
         return self.pure_mol_fr @ self.pure_enthalpy(units)
 
     def h_mix(self, units: str = "kJ/mol") -> NDArray[np.float64]:
-        """Enthalpy of mixing."""
+        """Enthalpy of mixing as a function of composition.
+        
+        Parameters
+        ----------
+        units: str
+            Enthalpy units (default: kJ/mol/K)
+            
+        Returns
+        -------
+        np.ndarray
+            1D array of mixing enthalpies as a function of composition.
+
+        .. math::
+            H_{mix} = H - H^{id}
+        
+        where: 
+            - :math:`H` is the simulation enthlapy for mixtures
+            - :math:`H^{id}` is ideal enthalpy 
+        """
         return self.enthalpy(units) - self.ideal_enthalpy(units)
 
     def molecule_rho(self, units: str = "molecule/nm^3") -> NDArray[np.float64]:
-        """Compute the number density of each molecule."""
+        """Compute the number density of each molecule for all compositions.
+        
+        Parameters
+        ----------
+        units: str
+            Number denisty units (default: molecule/nm^3)
+            
+        Returns
+        -------
+        np.ndarray
+            2D array of number density of each molecule as a function of composition.
+        """
         N_units, vol_units = units.split("/")  # get the target units
         N = self.Q_(self.molecule_counts, "molecule").to(N_units).magnitude
         V = self.volume(vol_units)[:, np.newaxis]
         return np.asarray(N / V)
 
     def volume_bar(self, units: str = "nm^3/molecule") -> NDArray[np.float64]:
-        """Linear combination of molar volumes."""
+        """Molar volume of mixture.
+
+        Parameters
+        ----------
+        units: str
+            Molar volume units (default: nm^3/molecule)
+            
+        Returns
+        -------
+        np.ndarray
+            1D array of molar volumes as a function of composition.
+        """
         return self.pure_mol_fr @ self.molar_volume(units)
 
     def rho_bar(self, units: str = "molecule/nm^3") -> NDArray[np.float64]:
-        """Linear combination of number density."""
+        """Number of mixture.
+
+        Parameters
+        ----------
+        units: str
+            Number density units (default: molecule/nm^3)
+            
+        Returns
+        -------
+        np.ndarray
+            1D array of number density as a function of composition.
+        """
         N_units, vol_units = units.split("/")
         return 1 / self.volume_bar(units=f"{vol_units}/{N_units}")
 
     def rho_ij(self, units: str = "molecule/nm^3") -> NDArray[np.float64]:
-        """Pairwise number density of molecules."""
+        """Pairwise number density of molecules.
+
+        Parameters
+        ----------
+        units: str
+            Number density units (default: molecule/nm^3)
+            
+        Returns
+        -------
+        np.ndarray
+            2D array of pairwise mixture number densities as a function of composition.
+        """
         return self.molecule_rho(units)[:, :, np.newaxis] * self.molecule_rho(units)[:, np.newaxis, :]
