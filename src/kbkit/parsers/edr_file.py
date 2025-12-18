95505730346dc194ad400526937b3bc4f1bfecb0
aperoutka <allison.peroutka98@gmail.com>
2025-12-18 15:51:20 -0600

    updated EdrParser

--- src/kbkit/parsers/edr_file.py
+++ src/kbkit/parsers/edr_file.py
@@ -268,7 +268,7 @@ class EdrFileParser:
             output_file = edr.with_name(f"isothermal_compressiblity_{edr.stem}.xvg")
             try:
                 result = subprocess.run(
-                    ["gmx", "energy", "-f", str(edr) "-b", int(start_time), "-o", str(output_file), "-fluct_props",],
+                    ["gmx", "energy", "-f", str(edr), "-b", int(start_time), "-o", str(output_file), "-fluct_props",],
                     input=input_props,
                     text=True,
                     capture_output=True,
